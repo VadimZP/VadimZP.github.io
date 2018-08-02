bc6cd9a5d30fe21f3475b3f196b58ceba95ce703
Vadim <wesborlandzp@gmail.com>
2018-08-02 21:23:10 +0300

    Optimization

--- cafe-landing/js/app.js
+++ cafe-landing/js/app.js
@@ -41,11 +41,11 @@
  })
 
  // Yandex map
- ymaps.ready(init);
+//  ymaps.ready(init);
 
- function init() {
-     var myMap = new ymaps.Map("map", {
-         center: [55.76, 37.64],
-         zoom: 7
-     });
- }
+//  function init() {
+//      var myMap = new ymaps.Map("map", {
+//          center: [55.76, 37.64],
+//          zoom: 7
+//      });
+//  }
